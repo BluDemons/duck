c49c2866c8d816cb04955e2ac1dc149414b302bc
BluDemons <jar.salazar@yavirac.edu.ec>
2020-01-31 11:51:45 -0500

    listo patos modelos

--- server/patosModelos.js
+++ server/patosModelos.js
@@ -2,12 +2,14 @@ const duck = require('./pato');
 
 class ModelPatos {
     patoMallard() {
+        console.log('Pato mallard');
         const mallard = new duck();
         mallard.setFlyBehavior().FlyWithWings();
         mallard.setFlyBehavior().fly();
         mallard.setQuackBehavior().quack();
     }
     patoCaucho() {
+        console.log('Pato de Caucho');
         const patoCaucho = new duck();
         patoCaucho.setFlyBehavior().FlyNoWay();
         patoCaucho.setQuackBehavior().noQuack();
